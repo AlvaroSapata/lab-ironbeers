d70ce53337958ca9e2694957213a362b808a26b5
Alvaro Martinez <alvaromfdv@gmail.com>
2023-05-03 19:16:26 +0200

    done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -14,12 +15,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
+//* PASOS PARA CREAR RUTAS
+// 1. Crear la ruta sin nada dentro
+// 2. Crear una vista con una estructura base (sin contenido)
+// 3. Rendereizar la vista en la ruta y la probamos
+// 4. Buscar la data
+// 5. Renderizar la data en el hbs {{}}
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(response => {
+      /* console.log(response); */
+      res.render('beers.hbs', {
+        allBeers: response
+      });
+    })
+    .catch(error => {
+      /* console.log(error); */
+    });
+});
+
+app.get("/random-beer",(req,res)=>{
+  punkAPI.getRandom()
+  .then(response => {
+    console.log(response);
+    res.render('random-beer.hbs', {
+      randomBeer: response,
+      
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+})
+
+app.get("/beers/:id",(req,res)=>{
+  const beer1 = punkAPI.getBeer(req.params.id)
+  .then((response)=>{
+    console.log(response)
+    res.render("eachBeer.hbs",{
+      beerInfo:response,
+    })
+  })
+  .catch(error => {
+    console.log(error);
+  });
+})
+
+//* PORT LISTENER
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
